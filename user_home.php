94d421070815db1bfbd2a0118ec07e4a85596e52
JaySao <khmersao1@gmail.com>
2018-03-17 18:25:10 -0400

    Added edit profile and fixed user home

--- user_home.php
+++ user_home.php
@@ -13,9 +13,19 @@ session_start();
 <?php
 	include("config.php");
 	print("<a href='home.html'>LOGOUT</a>");
-	print("<h1>Hello $_SESSION['Username'] </h1>");
+	print("<h1>Hello ".$_SESSION['Username']."</h1>");
+	//print("<h4>Your icon is: </h4>");
+	//print($_SESSION['Icon']);
+	//print("<h4>Your background is: </h4>")
+	//print($_SESSION['Background']);
 ?>
 </div>
+<a href="edit_profile.html">Edit Profile</a>
+
+<form action="change_status.php">
+	<input type="text" placeholder="How are you feeling?">
+	<input type="button" value="Change Status">
+</form>
 
 
 </body>
