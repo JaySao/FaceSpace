360d2d473a2b513e4607ef286599e36248c9f695
JaySao <khmersao1@gmail.com>
2018-03-18 22:18:08 -0400

    Added status change and wall posts to user home

---
+++ change_status.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Edit profile</title>
+	<link rel="stylesheet" href="./styler.css">
+</head>
+
+<body>
+<?php
+	session_start();
+	include("config.php");
+	print("Working");
+	$username = $_SESSION['Username'];
+	$status = $_POST['Status'];
+	print($username);
+	$sql = "UPDATE project3 SET Status='$status' WHERE Username='$username'";
+	if( mysql_query($sql)){
+		print("<br/>");
+		print("Updated");
+		print("You updated your status to: ");
+		print($status);
+		header('Location: user_home.php');
+	}else{
+		print("Error");
+		print("<br/><a href='user_home.php'>Return Home</a><br/>");
+	}
+	header();
+
+?>
+</body>
+</html>
