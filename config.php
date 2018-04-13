c691273341c84f0aa77a29f5186817fccd13334b
Jeremy Sao <khmersao1@gmail.com>
2018-04-13 17:55:09 -0400

    Added password confirmation

--- config.php
+++ config.php
@@ -9,18 +9,18 @@
 
 <?php
 	$host="localhost";
-	$username="abaskara1";
-	$pwd="abaskara1";
-	$db_name="abaskara1";
+	$username="jsao1";
+	$pwd="jsao1";
+	$db_name="jsao1";
 
 	$conn=mysql_connect("$host","$username","$pwd");
-	
+
 	if(!$conn){
-	
+
 		die("Cannot connect to server");
-		
+
 	}else{
-	
+
 		mysql_select_db("$db_name");
 		//echo "Connected to FaceSpace";
 		print ("<br/>");
--- register.php
+++ register.php
@@ -16,6 +16,8 @@
 	 $repassword = $_POST['repassword'];
 	 $answer1 = $_POST['question-1-answers'];
 	 $answer2 = $_POST['question-2-answers'];
+	 $secq = $_POST['secq'];
+	 $secanswer = $_POST['answer'];
 	 
 	 if ($answer1 == "A") { $icon="./bird.png"; }
 		else if ($answer1 == "B") { $icon="./snail.png"; }
@@ -37,7 +39,7 @@
 	}
 
 	print("<br/>");
-	$sql = "INSERT INTO project3 (Username, Password, Icon,Background,Status, Wall1,Wall2,Wall3,Wall4, Wall5,Wall6,Wall7,Wall8,Wall9,Wall10 ) VALUES('$regusername','$regpassword','$icon','$background','null','null','null','null','null','null','null','null','null','null','null')";
+	$sql = "INSERT INTO project3 (Username, Password, Icon,Background,Status, Wall1,Wall2,Wall3,Wall4, Wall5,Wall6,Wall7,Wall8,Wall9,Wall10,question,answer) VALUES('$regusername','$regpassword','$icon','$background','null','null','null','null','null','null','null','null','null','null','null','$secq','$secanswer')";
 	
 	if( mysql_query($sql)){
 		print("<br/>");
@@ -49,15 +51,10 @@
 	}
 	
 	//Testing below
-	 echo "$regusername";
+ 	echo "$secq";
 	 print("<br/>");
-	 echo "$regpassword";
-	 print("<br/>");
-	 echo "$repassword";
-	 print("<br/>");
-	 echo "$icon";
-	 print("<br/>");
-	echo "$background";
+	echo "$secanswer";
+
 		
 ?>
 </body>
