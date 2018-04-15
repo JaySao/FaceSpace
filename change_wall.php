1f754f1e8c3c40d471021392d3bcbfafb7ee0f03
Jeremy Sao <khmersao1@gmail.com>
2018-04-14 22:35:53 -0400

    Added new table for wall posts

--- change_wall.php
+++ change_wall.php
@@ -11,14 +11,14 @@
 	print("Working");
 	$fname = $_SESSION['fname'];
 	$wall = $_POST['wall'];
-	
-	$sql = "SELECT * FROM project3 WHERE Username='$fname'"; 
-	
-	
+
+	$sql = "SELECT * FROM wallposts WHERE Username='$fname'";
+
+
 	$result = mysql_query($sql);
-	
+
 	if(mysql_num_rows($result)>0){
-		
+
 			while($row = mysql_fetch_assoc($result)){
 				$wall1 =$row["Wall1"];
 				$wall2 =$row["Wall2"];
@@ -31,33 +31,33 @@
 				$wall9 =$row["Wall9"];
 				$wall10 =$row["Wall10"];
 				//Testing purposes below
-				
+
 			}
 	}
 	print ($wall1);
 	print($wall5);
 	if($wall1 =="null"){
-		$sql2 = "UPDATE project3 SET Wall1='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall1='$wall' WHERE Username='$fname'";
 	}else if ($wall2 =="null"){
-		$sql2 = "UPDATE project3 SET Wall2='$wall' WHERE Username='$fname'";
-	}else if ($wall3 =="null"){	
-		$sql2 = "UPDATE project3 SET Wall3='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall2='$wall' WHERE Username='$fname'";
+	}else if ($wall3 =="null"){
+		$sql2 = "UPDATE wallposts SET Wall3='$wall' WHERE Username='$fname'";
 	}else if ($wall4 =="null"){
-		$sql2 = "UPDATE project3 SET Wall4='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall4='$wall' WHERE Username='$fname'";
 	}else if ($wall5 =="null"){
-		$sql2 = "UPDATE project3 SET Wall5='$wall' WHERE Username='$fname'";	
+		$sql2 = "UPDATE wallposts SET Wall5='$wall' WHERE Username='$fname'";
 	}else if ($wall6 =="null"){
-		$sql2 = "UPDATE project3 SET Wall6='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall6='$wall' WHERE Username='$fname'";
 	}else if ($wall7 =="null"){
-		$sql2 = "UPDATE project3 SET Wall7='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall7='$wall' WHERE Username='$fname'";
 	}else if ($wall8 =="null"){
-		$sql2 = "UPDATE project3 SET Wall8='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall8='$wall' WHERE Username='$fname'";
 	}else if ($wall9 =="null"){
-		$sql2 = "UPDATE project3 SET Wall9='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall9='$wall' WHERE Username='$fname'";
 	}else {
-		$sql2 = "UPDATE project3 SET Wall10='$wall' WHERE Username='$fname'";
+		$sql2 = "UPDATE wallposts SET Wall10='$wall' WHERE Username='$fname'";
 	}
-	
+
 	if( mysql_query($sql2)){
 		header("Location: friend_home.php?fname=$fname");
 	}else{
@@ -65,7 +65,7 @@
 		print("<br/><a href='user_home.php'>Return Home</a><br/>");
 	}
 	header();
-	
+
 ?>
 </body>
 </html>
--- friend_home.php
+++ friend_home.php
@@ -14,7 +14,7 @@ include("config.php");
 $_SESSION['fname'] = $_GET['fname'];
 $friendname=$_SESSION['fname'];
 $originaluser = $_SESSION['Username'];
-$sql1 = "SELECT * FROM project3 WHERE Username ='$friendname'"; 
+$sql1 = "SELECT * FROM project3 WHERE Username ='$friendname'";
 
 
 		$result1 = mysql_query($sql1);
@@ -24,6 +24,14 @@ $sql1 = "SELECT * FROM project3 WHERE Username ='$friendname'";
 				$ficon = $row1["Icon"];
 				$fbackground =$row1["Background"];
 				$friendstat = $row1["Status"];
+			}
+		}
+$sql1 = "SELECT * FROM wallposts WHERE Username ='$friendname'";
+
+
+		$result1 = mysql_query($sql1);
+		if(mysql_num_rows($result1)>0){
+			while($row1 = mysql_fetch_assoc($result1)){
 				$fwall1 = $row1['Wall1'];
 				$fwall2 = $row1['Wall2'];
 				$fwall3 = $row1['Wall3'];
@@ -37,6 +45,7 @@ $sql1 = "SELECT * FROM project3 WHERE Username ='$friendname'";
 			}
 		}
 
+
 ?>
 <div class="pagewrap" style="background-image: url('<?php echo $fbackground;?>')">
 	<div id="navbar">
@@ -45,57 +54,46 @@ $sql1 = "SELECT * FROM project3 WHERE Username ='$friendname'";
 			<a href='user_home.php'>HOME</a>
 		<?php
 		print($flogged);
-		print("<h1>". $friendname." ");
-			if($flogged==0){
-				print("<img style='height:20px; width:20px;' id='logicon' src='./offline.png'></h1>");
-			}else{
-				print("<img style='height:20px; width:20px;' id='logicon' src='./online.png'></h1>");
-			}
+		print("<h1>". $friendname." </h1>");
 		//print("<h4>Your icon is: </h4>");
 		//print($_SESSION['Icon']);
 		//print("<h4>Your background is: </h4>")
 		//print($_SESSION['Background']);
 		print ("<img id='icon' src='".$ficon."'>");
-		$sql = "SELECT * FROM project3 WHERE Username !='$friendname'"; 
-	
+		$sql = "SELECT * FROM project3 WHERE Username !='$friendname'";
+
 		$result = mysql_query($sql);
-		
+
 print("<div class='row'>");
 	print("<div class='column left'>");
 		print("<h4>Their current status is: </h4>");
 		print("<h2>$friendstat</h2>");
 		print('<br>');
 		print("<h2>FRIENDS</h2>");
-		
+
 		if(mysql_num_rows($result)>0){
 			while($row = mysql_fetch_assoc($result)){
 				$friends = $row["Username"];
-				$friendLogged = $row["LoggedIn"];
 				if($friends == $originaluser){
-					
+
 				}else{
-				print("<div id='friends'><a href='friend_home.php?fname=$friends'>$friends</a>");
-					if($friendLogged==0){
-						print("<img style='height:20px; width:20px;' id='logicon' src='./offline.png'><br/> </div>");
-					}else{
-						print("<img style='height:20px; width:20px;' id='logicon' src='./online.png'><br/> </div>");
-					}
+				print("<div id='friends'><a href='friend_home.php?fname=$friends'>$friends</a><br/> </div>");
 				}
 			}
 		}
-				
 
-		
-	
-	
-		
+
+
+
+
+
 	print("</div>");
-	
+
 	print("<div class='column right'>");
-		print("<form action='change_wall.php' method='POST'>		
+		print("<form action='change_wall.php' method='POST'>
 				<input type='text' placeholder='Write on Wall' name='wall'>
 				<input type='submit' value='Write!'>
-		</form>");	
+		</form>");
 		print('<h4>Wall posts are: </h4>');
 		//TODO: Add if statement, if(null)=>Do not print
 		print('<br>');
@@ -139,9 +137,9 @@ print("<div class='row'>");
 		print("<h3>$fwall10</h3>");
 		}
 	print("</div>");
-print("</div>");	
+print("</div>");
 		?>
-		
+
 		</div>
 	</div>
 </div>
